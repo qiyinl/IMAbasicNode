dab0d8e5cee835ad59d146ca536a9caf3ba3f35c
AWG <awg@Qiyins-MBP.localdomain>
2024-08-23 13:38:49 -0300

    basic IMA HTML SDK project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const fs =require('fs')
+const app = express();
+const path = require('path');
+const { send } = require('process');
+
+app.use(express.static(path.resolve(__dirname,'./public')));
+
+app.set('port',process.env.PORT || 3000)
+
+app.listen(app.get('port'),() => console.log('Server start http://localhost:' +app.get('port')));
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views','index.html'))
+
+})
